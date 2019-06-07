e455909805aca219d540720660dd801b196c3ff8
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-07 17:37:29 -0600

    Deleted and adding styling in the App.css file. Wrote the initial code in the PostForm component. Imported PostForm in the App.js file.

---
+++ redux-example/src/components/PostForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PostForm extends React.Component {
+
+	state = {
+
+	};
+
+	render () {
+		return (
+			<div>
+				<h1>Add Post</h1>
+			</div>
+		)
+	}
+}
+
+export default PostForm;
