90da2a6b2c2b32346d5fe641e0087d4c904017f5
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-07 19:22:57 -0600

    Added an hr tag in the App.js file. In the PostForm component, set the initial state, created a bind for 'this', wrote an onChange function, and put JSX in the render method.

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
   	<div className="App">
 		<PostForm />
+		<hr />
       <Posts />
 	</div>
   );
--- redux-example/src/components/PostForm.js
+++ redux-example/src/components/PostForm.js
@@ -3,13 +3,46 @@ import React from 'react';
 class PostForm extends React.Component {
 
 	state = {
-
+		title: '',
+		body: ''
 	};
+
+	//Bind this.
+	//Note: this statement used to go inside of the constructor declaration.
+	onChange = this.onChange.bind(this);
+
+	onChange(e) {
+		//Set the state.
+		//Use e.target to avoid having to create separate onChange functions for every event.
+		this.setState({[e.target.name]: e.target.value})
+	}
 
 	render () {
 		return (
 			<div>
 				<h1>Add Post</h1>
+				<form>
+					<div>
+						<label>Title:</label><br />
+						<input
+							type='text'
+							name='title'
+							onChange={this.onChange}
+							value={this.state.title}
+						/>
+					</div>
+					<br />
+					<div>
+						<label>Body:</label><br />
+						<textarea
+							name='body'
+							onChange={this.onChange}
+							value={this.state.body}
+						/>
+					</div>
+					<br />
+					<button type='submit'>Submit</button>
+				</form>
 			</div>
 		)
 	}
